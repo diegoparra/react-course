65b1a82106b845dd2c7938844dd53047f182c506
Diego Parra <diegoparraferreira@gmail.com>
2023-05-16 15:39:27 -0400

    resolvendo warning da propriedade key

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -4,11 +4,23 @@ const ListRender = () => {
 
   const [list] = useState(["Liz", "joao", "Carla"]);
 
+  const [users] = useState([
+    { id: 1, name: "Cida", age: 79 },
+    { id: 3, name: "Carlos", age: 55 },
+    { id: 4, name: "Marcerlo", age: 38 },
+  ]);
+
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+
+      <ul>
+        {users.map((users) => (
+          <li key={users.id}>nome: {users.name}, idade: {users.age}</li>
         ))}
       </ul>
     </div >

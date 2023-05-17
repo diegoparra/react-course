26f200e2ad5a08b94e887341dc2c223ce963ef9c
Diego Parra <diegoparraferreira@gmail.com>
2023-05-17 11:02:43 -0400

    praticando conditional render

--- src/components/ConditionalRender.js
+++ src/components/ConditionalRender.js
@@ -3,11 +3,29 @@ import { useState } from "react"
 const ConditionalRender = () => {
 
   const [x] = useState(true);
+
+  const [name, setName] = useState("Joao");
+
   return (
     <div>
       <h1>Isso Sera Exibido?</h1>
       {x && <p>Se X for true, sera exibido!</p>}
       {!x && <p>Agora o X eh false</p>}
+      <h1>If ternario</h1>
+      <div>
+        {name === "Diego" ? (
+          <div>
+            <p>Nome encontrado, {name} </p>
+          </div>
+        ) : (
+          <div>
+            <p>Nome nao encontrado.</p>
+          </div>
+        )}
+      </div>
+      <div>
+        <button onClick={() => setName("Diego")}>Aplicar nome Diego</button>
+      </div>
     </div>
   )
 }
